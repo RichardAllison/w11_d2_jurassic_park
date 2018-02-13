cce57d52c7f602a2aede2e7efad6b8da7591b9e3
Richard Allison <richard.allison.18@outlook.com>
2018-02-13 20:34:19 +0000

    able to get all dinosaurs with offspring more than 2

--- park.js
+++ park.js
@@ -8,13 +8,22 @@ Park.prototype.addDinosaur = function(dinosaur) {
 
 Park.prototype.removeDinosaursOfType = function(dinosaurType) {
   for (var dinosaur of this.enclosure) {
-    if (dinosaur.type == dinosaurType) {
+    if (dinosaur.type === dinosaurType) {
       const index = this.enclosure.indexOf(dinosaur);
       if (index >= 0) {
         this.enclosure.splice(index,1)
       }
     }
   }
+}
+Park.prototype.allOverTwoOffspring = function() {
+  const dinosaurs = [];
+  for (var dinosaur of this.enclosure) {
+    if (dinosaur.numberOfOffspring > 2) {
+      dinosaurs.push(dinosaur);
+    }
+  }
+  return dinosaurs;
 }
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -52,4 +52,14 @@ describe('Park', function() {
     assert.deepStrictEqual(park.enclosure, [dinosaur1, dinosaur3, dinosaur4]);
   });
 
+  it('should be able to get all dinosaurs with offspring more than 2', function() {
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.addDinosaur(dinosaur5);
+    actual = park.allOverTwoOffspring();
+    assert.deepStrictEqual(actual, [dinosaur2, dinosaur3, dinosaur4]);
+  });
+
 });
