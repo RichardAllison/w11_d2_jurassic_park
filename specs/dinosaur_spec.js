1203d837b0590329b584fd83af8b655faad494ec
Richard Allison <richard.allison.18@outlook.com>
2018-02-13 19:58:51 +0000

    dinosaur has number of offspring per year

--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -6,12 +6,17 @@ describe('Dinosaur', function() {
   let dinosaur;
 
   beforeEach(function() {
-    dinosaur = new Dinosaur('Tyrannosaurus Rex');
+    dinosaur = new Dinosaur('Tyrannosaurus Rex', 2);
   });
 
   it('should have a type', function() {
     actual = dinosaur.type;
     assert.strictEqual(actual, 'Tyrannosaurus Rex');
+  });
+
+  it('should have a number of offspring per year', function() {
+    actual = dinosaur.numberOfOffspring;
+    assert.strictEqual(actual, 2);
   });
 
 });
